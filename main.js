b184a4b310bd34ebde0c82c31814815ae3306f37
aguilerasergio <sergioaguilera1821@gmail.com>
2024-01-08 22:18:34 -0300

    agregue el login

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+function logear()
+{
+let user=document.getElementById("usuario").value;
+let pass=document.getElementById("clave").value;
+
+if(user=="sergio" && pass=="1234")
+{
+
+    window.location="Enmarcaciones.html";
+
+} 
+
+ else
+    {
+
+        alert("Datos incorrectos");
+
+    }
+
+
+}
